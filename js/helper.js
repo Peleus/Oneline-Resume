df70cec6e31a65a052f96edfc27fe44484b04944
Brett Freese <Brett Freese>
2016-01-08 09:48:29 -0500

    * Revise comments

--- js/helper.js
+++ js/helper.js
@@ -1,17 +1,4 @@
-/*
-
-This file contains all of the code running in the background that makes resumeBuilder.js possible. We call these helper functions because they support your code in this course.
-
-Don't worry, you'll learn what's going on in this file throughout the course. You won't need to make any changes to it until you start experimenting with inserting a Google Map in Problem Set 3.
-
-Cameron Pittman
-*/
-
-
-/*
-These are HTML strings. As part of the course, you'll be using JavaScript functions
-replace the %data% placeholder text you see in them.
-*/
+//vars for resume sections and text %data% to replace with jQuery
 var HTMLheaderName = '<h1 id="name">%data%</h1>';
 var HTMLheaderRole = '<span>%data%</span><hr>';
 
@@ -55,27 +42,7 @@ var HTMLonlineSchool = ' - %data%</a>';
 var HTMLonlineDates = '<div class="date-text">%data%</div>';
 var HTMLonlineURL = '<br><a href="#">%data%</a>';
 
-//var internationalizeButton = '<button>Internationalize</button>';
 var googleMap = '<div id="map"></div>';
-
-
-/*
-The International Name challenge in Lesson 2 where you'll create a function that will need this helper code to run. Don't delete! It hooks up your code to the button you'll be appending.
-*/
-/*$(document).ready(function() {
-  $('button').click(function() {
-    var iName = inName() || function(){};
-    $('#name').html(iName);  
-  });
-});
-
-function inName(name){
-  console.log(name);
-  //var nameSplit = name.split(" ");
-  //console.log(nameSplit);
-  //return nameSplit;
-}*/
-
 
 /*
 The next few lines about clicks are for the Collecting Click Locations quiz in Lesson 2.
@@ -92,18 +59,7 @@ function logClicks(x,y) {
   console.log('x location: ' + x + '; y location: ' + y);
 }
 
-// This function is called from resume builder
-/*$(document).click(function(loc) {
-    logClicks(loc.pageX,loc.pageY);
-});*/
-
-
-
-/*
-This is the fun part. Here's where we generate the custom Google Map for the website.
-See the documentation below for more details.
-https://developers.google.com/maps/documentation/javascript/reference
-*/
+// Set up the Map
 var map;    // declares a global map variable
 
 
@@ -179,11 +135,6 @@ function initializeMap() {
       content: name
     });
 
-    // hmmmm, I wonder what this is about...
-    google.maps.event.addListener(marker, 'click', function() {
-      // your code goes here!
-    });
-
     // this is where the pin actually gets added to the map.
     // bounds.extend() takes in a map location object
     bounds.extend(new google.maps.LatLng(lat, lon));
@@ -240,7 +191,7 @@ function initializeMap() {
 }
 
 /*
-Uncomment the code below when you're ready to implement a Google Map!
+Publishes the map
 */
 
 // Calls the initializeMap() function when the page loads
